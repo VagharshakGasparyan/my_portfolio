a85981db8f0e50d87bdec009aa1a53365fd176ff
VagharshakGasparyan <vvagharshak@gmail.com>
2025-08-23 15:07:23 +0400

    add cogwheel

--- js/admin.js
+++ js/admin.js
@@ -6,15 +6,33 @@ window.addEventListener('load', () => {
         aside.classList.toggle('aside-to-left');
         document.body.classList.toggle('body-aside-to-left');
     });
-    let dashboard_text = document.getElementById('dashboard_text');
+    let shadow_text = document.getElementById('shadow_text');
     document.body.addEventListener('mousemove', (ev)=>{
         // console.log(ev.pageX, ev.pageY);
-        let x0 = dashboard_text.getBoundingClientRect().left + dashboard_text.getBoundingClientRect().width / 2;
-        let y0 = dashboard_text.getBoundingClientRect().top + dashboard_text.getBoundingClientRect().height / 2;
+        let x0 = shadow_text.getBoundingClientRect().left + shadow_text.getBoundingClientRect().width / 2;
+        let y0 = shadow_text.getBoundingClientRect().top + shadow_text.getBoundingClientRect().height / 2;
         let x1 = ev.pageX;
         let y1 = ev.pageY;
         let r = Math.sqrt((x0 - x1) * (x0 - x1) + (y0 - y1) * (y0 - y1));
-        dashboard_text.style.textShadow = ((x0 - x1) / 2).toFixed(2) + 'px '
-            + ((y0 - y1) / 2).toFixed(2) + 'px ' + (r / 15 + 2).toFixed(2) + 'px white';
+        shadow_text.style.textShadow = ((x0 - x1) / 2).toFixed(2) + 'px '
+            + ((y0 - y1) / 2).toFixed(2) + 'px ' + (r / 20 + 2).toFixed(2) + 'px white';
+    });
+    let main = document.querySelector('main');
+    let zk = document.getElementById('zk');
+    let zk_k = {
+        k: 2.507,
+        start: 248,
+        end: 535
+    };
+    main.addEventListener('scroll', (ev) => {
+        let dx = main.scrollTop;
+        let a = 0;
+        if (dx > zk_k.start && dx < zk_k.end) {
+            a = (dx - zk_k.start) * zk_k.k;
+        }else if(dx >= zk_k.end){
+            a = (zk_k.end - zk_k.start) * zk_k.k;
+        }
+        zk.style.transform = 'rotate(' + a + 'deg)';
+        // console.log(dx, a);
     });
 });
